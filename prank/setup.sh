c8f2fb216dee65611da97c27502141afdb4395cc
BoppreH <boppreh@gmail.com>
2014-04-24 16:02:01 -0300

    Use printf instead of echo

--- prank/setup.sh
+++ prank/setup.sh
@@ -5,5 +5,5 @@ cd ~/.hue
 wget http://boppreh.com/prank/deploy.zip
 unzip deploy.zip
 chmod +x deploy/espeak
-echo -e "\n\npython ~/.hue/deploy/hue_server.py" >> ~/.bash_profile
+printf "\n\npython ~/.hue/deploy/hue_server.py" >> ~/.bash_profile
 python ~/.hue/deploy/hue_server.py &
